d4124b4371998605acf628b7471a2ed65dd7c905
AyyanNadeem01 <ayyan137a@gmail.com>
2025-08-14 23:50:15 +0500

    Signup page done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDb from "./config/db.js"
 import authRouter from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 dotenv.config()
 const app=express()
 
@@ -10,7 +11,9 @@ const app=express()
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
-app.use
+app.use(cors({
+    origin:"http://localhost:5173", credentials:true
+}))
 app.use("/api/auth",authRouter)
 
 
