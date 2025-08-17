111f208a5575ce52b3c15de161cd2d6ed516850e
Ayyan Nadeem <ayyan137a@gmail.com>
2025-08-17 19:15:48 +0500

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -56,8 +56,8 @@ export const logIn = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "non",
+            secure: true
         });
 
         const { password: _, ...userData } = user.toObject();
