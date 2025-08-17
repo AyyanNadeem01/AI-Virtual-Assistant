ce24a8f2357cad9175b93efe164b7e63c0589a19
Ayyan Nadeem <ayyan137a@gmail.com>
2025-08-17 19:06:19 +0500

    Update Customize2.jsx

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { UserDataContext } from '../context/userContext'
+import { UserDataContext } from '../context/UserContext'
 import { IoArrowBack } from "react-icons/io5"
 import { useNavigate } from "react-router-dom";
 import axios from "axios"
