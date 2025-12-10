a123328e2fc5d7eb7d8dae9bb013d8f83b1a27bf
Ayyan Nadeem <ayyan137a@gmail.com>
2025-12-11 04:34:14 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDb=async()=>{
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("Database Connected Successfully")
     }catch(error){
-
+    console.log("DB Connection failed!", error)
     }
 }
 
